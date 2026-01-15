a971e630e5fff589a8b730b37bc258d9761009ef
veereshsajjansajjan1-sv <veereshsajjansajjan1@gmail.com>
2026-01-15 12:27:07 +0530

    save

--- cal.py
+++ cal.py
@@ -1,17 +1,22 @@
 import sys
 
-if len(sys.argv)>0:
-    script_name=sys.argv[0]
-    num1=sys.argv[1]
-    num2=sys.argv[2]
+if len(sys.argv) > 2:
+    script_name = sys.argv[0]
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
 else:
-    script_name=sys.argv[0]
-    num1=162
-    num2=153
+    script_name = sys.argv[0]
+    num1 = 162
+    num2 = 153
 
 print("calculator")
-sum=num1+num2
-sub=num1-num2
-mul=num1*num2
-div=num1/num2
-print(f"addition:{sum}\nsubtraction:{sub}\nmultiplication:{mul}\ndivision:{div}")
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(f"addition: {sum}")
+print(f"subtraction: {sub}")
+print(f"multiplication: {mul}")
+print(f"division: {div}")
